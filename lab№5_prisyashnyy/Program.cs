7ee674d9935074a2919bea789f3389b0dada0784
prisy <prisy@LAPTOP-23R98U4U>
2023-10-04 16:51:11 +0400

    Добавьте файлы проекта.

---
+++ lab№5_prisyashnyy/Program.cs
@@ -0,0 +1,18 @@
+﻿//lab№5/Сборник задач/Разветвляющиеся алгоритмы/условные операторы/высокий уровень/вариант 19
+Console.Write("Введите сторону кирпича a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите сторону кирпича b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите сторону кирпича c: ");
+int c = int.Parse(Console.ReadLine());
+Console.Write("Введите сторону отверстия x: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите сторону отверстия y: ");
+int y = int.Parse(Console.ReadLine());
+if ((a <= x && b <= y) || (a <= y && b <= x) || (a <= x && c <= y) || (a <= y && c <= x) || (b <= x && c <= y) || (b <= y && c <= x))
+{
+    Console.WriteLine("кирпич пройдёт");
+} else
+{
+    Console.WriteLine("кирпич не проёдут благаду 27 года 19 века");
+}
